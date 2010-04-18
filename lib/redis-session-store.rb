83720ebaed91d004dc5dcea6d768c36cf2777750
Ivan Porto Carrero <ivan@flanders.co.nz>
2010-04-18 11:36:56 +0200

    Compatibility with rails3

--- lib/redis-session-store.rb
+++ lib/redis-session-store.rb
@@ -1,64 +1,69 @@
 require 'redis'
 
-# Redis session storage for Rails, and for Rails only. Derived from
-# the MemCacheStore code, simply dropping in Redis instead.
-#
-# Options:
-#  :key     => Same as with the other cookie stores, key name
-#  :secret  => Encryption secret for the key
-#  :host    => Redis host name, default is localhost
-#  :port    => Redis port, default is 6379
-#  :db      => Database number, defaults to 0. Useful to separate your session storage from other data
-#  :key_prefix  => Prefix for keys used in Redis, e.g. myapp-. Useful to separate session storage keys visibly from others
-#  :expire_after => A number in seconds to set the timeout interval for the session. Will map directly to expiry in Redis
+module ActionDispatch
+  module Session
 
-class RedisSessionStore < ActionController::Session::AbstractStore
+    # Redis session storage for Rails, and for Rails only. Derived from
+    # the MemCacheStore code, simply dropping in Redis instead.
+    #
+    # Options:
+    #  :key     => Same as with the other cookie stores, key name
+    #  :secret  => Encryption secret for the key
+    #  :host    => Redis host name, default is localhost
+    #  :port    => Redis port, default is 6379
+    #  :db      => Database number, defaults to 0. Useful to separate your session storage from other data
+    #  :key_prefix  => Prefix for keys used in Redis, e.g. myapp-. Useful to separate session storage keys visibly from others
+    #  :expire_after => A number in seconds to set the timeout interval for the session. Will map directly to expiry in Redis
 
-  def initialize(app, options = {})
-    # Support old :expires option
-    options[:expire_after] ||= options[:expires]
+    class RedisSessionStore < AbstractStore
 
-    super
+      def initialize(app, options = {})
+        # Support old :expires option
+        options[:expire_after] ||= options[:expires]
 
-    @default_options = {
-      :namespace => 'rack:session',
-      :host => 'localhost',
-      :port => '6379',
-      :db => 0,
-      :key_prefix => ""
-    }.update(options)
+        super
 
-    @pool = Redis.new(@default_options)
-  end
+        @default_options = {
+          :namespace => 'rack:session',
+          :host => 'localhost',
+          :port => '6379',
+          :db => 0,
+          :key_prefix => ""
+        }.update(options)
 
-  private
-    def prefixed(sid)
-      "#{@default_options[:key_prefix]}#{sid}"
-    end
+        @pool = Redis.new(@default_options)
+      end
+
+      private
+        def prefixed(sid)
+          "#{@default_options[:key_prefix]}#{sid}"
+        end
     
-    def get_session(env, sid)
-      sid ||= generate_sid
-      begin
-        data = @pool.call_command([:get, prefixed(sid)])
-        session = data.nil? ? {} : Marshal.load(data)
-      rescue Errno::ECONNREFUSED
-        session = {}
-      end
-      [sid, session]
-    end
+        def get_session(env, sid)
+          sid ||= generate_sid
+          begin
+            data = @pool.call_command([:get, prefixed(sid)])
+            session = data.nil? ? {} : Marshal.load(data)
+          rescue Errno::ECONNREFUSED
+            session = {}
+          end
+          [sid, session]
+        end
 
-    def set_session(env, sid, session_data)
-      options = env['rack.session.options']
-      expiry  = options[:expire_after] || nil
+        def set_session(env, sid, session_data)
+          options = env['rack.session.options']
+          expiry  = options[:expire_after] || nil
       
-      @pool.pipelined do |redis|
-        redis.set(prefixed(sid), Marshal.dump(session_data))
-        redis.expire(prefixed(sid), expiry) if expiry
-      end
+          @pool.pipelined do |redis|
+            redis.set(prefixed(sid), Marshal.dump(session_data))
+            redis.expire(prefixed(sid), expiry) if expiry
+          end
         
-      return true
-    rescue Errno::ECONNREFUSED
-      return false
-    end
+          return true
+        rescue Errno::ECONNREFUSED
+          return false
+        end
   
+    end
+end
 end
